aef4e416b5f0bdf522e759b29d7261d544d925f4
Sad8487 <svetlana.dyablo@gmail.com>
2025-01-05 22:27:50 +0300

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,19 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-//
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: ['babel-plugin-styled-components'],
+        plugins: ["babel-plugin-styled-components"],
       },
     }),
   ],
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
+  },
+  build: {
+    outDir: "build",
   },
 });
